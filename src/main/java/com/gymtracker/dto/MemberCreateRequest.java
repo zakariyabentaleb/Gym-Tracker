778322e1d3d53673d46660af76b0922bcdbbb008
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-02-08 16:23:12 +0100

    Add MemberCreateRequest.java

---
+++ src/main/java/com/gymtracker/dto/MemberCreateRequest.java
@@ -0,0 +1,79 @@
+package com.gymtracker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class MemberCreateRequest {
+
+    @NotNull
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 80)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 80)
+    private String lastName;
+
+    @Size(max = 30)
+    private String phone;
+
+    @Past
+    private LocalDate birthDate;
+
+    private Boolean active = true;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
+
