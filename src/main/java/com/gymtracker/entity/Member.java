71acf306b0043a907bf5c4ef3a4d2c66085e8274
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-02-08 16:23:13 +0100

    Add Member.java

--- src/main/java/com/gymtracker/entity/Member.java
+++ src/main/java/com/gymtracker/entity/Member.java
@@ -1,4 +1,44 @@
 package com.gymtracker.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("members")
 public class Member {
+
+    @Id
+    @Column("id")
+    private Long id;
+
+    /**
+     * Link to users.id.
+     */
+    @Column("user_id")
+    private Long userId;
+
+    @Column("first_name")
+    private String firstName;
+
+    @Column("last_name")
+    private String lastName;
+
+    @Column("phone")
+    private String phone;
+
+    @Column("birth_date")
+    private LocalDate birthDate;
+
+    @Column("active")
+    private Boolean active;
 }
