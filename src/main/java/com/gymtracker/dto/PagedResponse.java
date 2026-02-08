e2411e7a9a4926f6ef81123fda8e02a0a59d00ae
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-02-08 16:23:13 +0100

    Add PagedResponse.java

---
+++ src/main/java/com/gymtracker/dto/PagedResponse.java
@@ -0,0 +1,53 @@
+package com.gymtracker.dto;
+
+import java.util.List;
+
+public class PagedResponse<T> {
+    private List<T> items;
+    private long total;
+    private int page;
+    private int size;
+
+    public PagedResponse() {
+    }
+
+    public PagedResponse(List<T> items, long total, int page, int size) {
+        this.items = items;
+        this.total = total;
+        this.page = page;
+        this.size = size;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void setItems(List<T> items) {
+        this.items = items;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
+
