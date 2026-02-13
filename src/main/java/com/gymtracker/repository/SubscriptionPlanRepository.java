1f489ade3302ac005156680296b86d78092375b7
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-02-13 15:03:57 +0100

    Add SubscriptionPlanRepository.java

---
+++ src/main/java/com/gymtracker/repository/SubscriptionPlanRepository.java
@@ -0,0 +1,8 @@
+package com.gymtracker.repository;
+
+import com.gymtracker.entity.SubscriptionPlan;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriptionPlanRepository extends CrudRepository<SubscriptionPlan, Long> {
+}
+
