938733e170452d5721b0d7e4fcdafebac02335aa
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-09 02:23:21 +0000

    Add BookingStatus.java

---
+++ src/main/java/com/gymtracker/service/BookingStatus.java
@@ -0,0 +1,10 @@
+package com.gymtracker.service;
+
+public final class BookingStatus {
+    private BookingStatus() {}
+
+    public static final String CONFIRMED = "CONFIRMED";
+    public static final String CANCELLED = "CANCELLED";
+    public static final String WAITING = "WAITING";
+}
+
