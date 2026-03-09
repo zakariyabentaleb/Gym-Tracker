e0086bd13d4ea93ee3ef8d29d97dcfedf5182868
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-09 12:40:24 +0000

    Add MeResponse.java

--- src/main/java/com/gymtracker/dto/MeResponse.java
+++ src/main/java/com/gymtracker/dto/MeResponse.java
@@ -2,6 +2,6 @@ package com.gymtracker.dto;
 
 import java.util.List;
 
-public record MeResponse(String username, List<String> roles) {
+public record MeResponse(Long id, String username, List<String> roles) {
 }
 
