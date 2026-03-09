3b1de431ee92120947971c7802b6807c3c7ae4ac
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-09 02:23:11 +0000

    Add CoachCreateRequest.java

---
+++ src/main/java/com/gymtracker/dto/CoachCreateRequest.java
@@ -0,0 +1,18 @@
+package com.gymtracker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CoachCreateRequest {
+    private Long userId;
+
+    @NotBlank
+    private String displayName;
+
+    private String phone;
+    private String bio;
+    private String certifications;
+    private String photoUrl;
+}
+
