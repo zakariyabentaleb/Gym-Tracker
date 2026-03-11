df7ea1461036bfb1887a723beabe5b12a0e84179
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-11 11:32:44 +0000

    Add ChangePasswordRequest.java

---
+++ src/main/java/com/gymtracker/dto/ChangePasswordRequest.java
@@ -0,0 +1,16 @@
+package com.gymtracker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequest {
+
+    @NotBlank
+    private String currentPassword;
+
+    @NotBlank
+    @Size(min = 4, max = 100)
+    private String newPassword;
+}
