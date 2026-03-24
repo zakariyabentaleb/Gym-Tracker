baafb38c7a17b97f73c753c9dca7ff587fc48625
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 21:05:08 +0100

    Add SubscriptionRepository.java

--- src/main/java/com/gymtracker/repository/SubscriptionRepository.java
+++ src/main/java/com/gymtracker/repository/SubscriptionRepository.java
@@ -1,19 +1,19 @@
 package com.gymtracker.repository;
 
 import com.gymtracker.entity.Subscription;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SubscriptionRepository extends CrudRepository<Subscription, Long> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
-    @Query("SELECT * FROM subscriptions WHERE member_id = :memberId")
+    @Query(value = "SELECT * FROM subscriptions WHERE member_id = :memberId", nativeQuery = true)
     List<Subscription> findByMemberId(@Param("memberId") Long memberId);
 
-    @Query("SELECT * FROM subscriptions ORDER BY id DESC")
+    @Query(value = "SELECT * FROM subscriptions ORDER BY id DESC", nativeQuery = true)
     List<Subscription> findAllOrderByIdDesc();
 }
