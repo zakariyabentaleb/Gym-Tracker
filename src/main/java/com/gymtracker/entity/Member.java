c960208d38c2216fe73caeca55630e99ccc088d8
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 21:05:03 +0100

    Add Member.java

--- src/main/java/com/gymtracker/entity/Member.java
+++ src/main/java/com/gymtracker/entity/Member.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.time.LocalDate;
 
@@ -14,31 +17,33 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table("members")
+@Entity
+@Table(name = "members")
 public class Member {
 
     @Id
-    @Column("id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     /**
      * Link to users.id.
      */
-    @Column("user_id")
+    @Column(name = "user_id")
     private Long userId;
 
-    @Column("first_name")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column("last_name")
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column("phone")
+    @Column(name = "phone")
     private String phone;
 
-    @Column("birth_date")
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
-    @Column("active")
+    @Column(name = "active")
     private Boolean active;
 }
