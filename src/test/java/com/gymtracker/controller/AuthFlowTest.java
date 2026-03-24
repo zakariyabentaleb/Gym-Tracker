f0ec2a328474dd6156e896d1fa245980cd620fd0
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 21:24:00 +0100

    Add AuthFlowTest.java

--- src/test/java/com/gymtracker/controller/AuthFlowTest.java
+++ src/test/java/com/gymtracker/controller/AuthFlowTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -81,3 +82,4 @@ class AuthFlowTest {
                 .andExpect(status().isOk());
     }
 }
+
