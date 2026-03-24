acc16c359fed3c1ff3625a831b1129b0aee530fe
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 21:05:06 +0100

    Add CourseRepository.java

--- src/main/java/com/gymtracker/repository/CourseRepository.java
+++ src/main/java/com/gymtracker/repository/CourseRepository.java
@@ -1,10 +1,9 @@
 package com.gymtracker.repository;
 
 import com.gymtracker.entity.Course;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
-
