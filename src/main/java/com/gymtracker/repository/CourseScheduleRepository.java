8c5443fe008f49deab3d423e781434d741ea6761
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 21:05:07 +0100

    Add CourseScheduleRepository.java

--- src/main/java/com/gymtracker/repository/CourseScheduleRepository.java
+++ src/main/java/com/gymtracker/repository/CourseScheduleRepository.java
@@ -1,18 +1,19 @@
 package com.gymtracker.repository;
 
 import com.gymtracker.entity.CourseSchedule;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CourseScheduleRepository extends CrudRepository<CourseSchedule, Long> {
-    @Query("SELECT * FROM course_schedules WHERE course_id = :courseId")
+public interface CourseScheduleRepository extends JpaRepository<CourseSchedule, Long> {
+
+    @Query(value = "SELECT * FROM course_schedules WHERE course_id = :courseId", nativeQuery = true)
     List<CourseSchedule> findByCourseId(@Param("courseId") Long courseId);
 
-    @Query("SELECT * FROM course_schedules WHERE coach_id = :coachId")
+    @Query(value = "SELECT * FROM course_schedules WHERE coach_id = :coachId", nativeQuery = true)
     List<CourseSchedule> findByCoachId(@Param("coachId") Long coachId);
 }
