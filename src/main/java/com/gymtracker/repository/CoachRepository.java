a9db8b74192a0faadb6907a49f5b33b39e44b0ab
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-03-24 21:05:06 +0100

    Add CoachRepository.java

--- src/main/java/com/gymtracker/repository/CoachRepository.java
+++ src/main/java/com/gymtracker/repository/CoachRepository.java
@@ -1,19 +1,16 @@
 package com.gymtracker.repository;
 
 import com.gymtracker.entity.Coach;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CoachRepository extends CrudRepository<Coach, Long> {
-    @Query("SELECT * FROM coaches WHERE active = true")
-    List<Coach> findActive();
+public interface CoachRepository extends JpaRepository<Coach, Long> {
+    // Derived query methods for JPA
+    List<Coach> findByActiveTrue();
 
-    @Query("SELECT * FROM coaches WHERE user_id = :userId LIMIT 1")
     Optional<Coach> findByUserId(Long userId);
 }
-
